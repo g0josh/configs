08df4270e0640ec3c99df1987d01372f334ded7f
Jobin <jobin.jj@gmail.com>
2019-08-01 14:55:42 +1000

    back to i3+polybar. TODO:rofi/dmenu

---
+++ installi3.sh
@@ -0,0 +1,21 @@
+
+sudo apt install libxcb-xrm-dev libxcb1-dev libxcb-keysyms1-dev libpango1.0-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libxcb-util0-dev libxcb-icccm4-dev libyajl-dev libstartup-notification0-dev libxcb-randr0-dev libev-dev libxcb-cursor-dev libxcb-xinerama0-dev libxcb-xkb-dev libxkbcommon-dev libxkbcommon-x11-dev autoconf libxcb-xrm0 libxcb-xrm-dev automake libxcb-shape0-dev
+cd ~/Downloads
+git clone https://www.github.com/Airblader/i3 i3-gaps
+git checkout gaps
+cd i3-gaps
+autoreconf --force --install
+rm -rf build/
+mkdir -p build && cd build/
+../configure --prefix=/usr --sysconfdir=/etc --disable-sanitizers
+make
+sudo make install
+
+apt install build-essential git cmake cmake-data pkg-config python3-sphinx libcairo2-dev libxcb1-dev libxcb-util0-dev libxcb-randr0-dev libxcb-composite0-dev python-xcbgen xcb-proto libxcb-image0-dev libxcb-ewmh-dev libxcb-icccm4-dev libxcb-cursor-dev libasound2-dev libmpdclient-dev libiw-dev libnl-genl-3-dev libpulse-dev
+cd ~/Downloads
+git clone --recursive https://github.com/polybar/polybar
+git checkout 3.4
+cd polybar
+bash build.sh
+
+
