4eff939d17bae43a383bad9880429dc77c4dfd4d
Jobin <jobin.jj@gmail.com>
2019-08-02 16:33:30 +1000

    better cpu utilization

--- .config/polybar/utilization.sh
+++ .config/polybar/utilization.sh
@@ -1,4 +1,4 @@
-usage=`top -bn2 -d0.1 |grep 'load average'|tail -n1|awk '{print $10}'|cut -c -4`
+if [[ `top -bn2 -d0.1 |grep 'load average'|tail -n1` =~ [0-9]\.[0-9]{2} ]];then usage="${BASH_REMATCH}"; fi
 cores=`cat /proc/cpuinfo|grep 'cpu cores'|tail -n1|awk '{print $4}'`
 cpuusage=$(echo "${usage}*100/${cores}"|bc)
 gpuusage=`nvidia-smi -q -d UTILIZATION|grep Gpu|awk '{print $3}'`
