06600d134dd076b2e12e4189dbde80896e85045b
job <jobin.jj@gmail.com>
2019-08-27 12:51:56 +1000

    power module for revamped poly

--- .config/i3/detect_monitors_load_bar.py
+++ .config/i3/detect_monitors_load_bar.py
@@ -5,6 +5,8 @@ import subprocess
 import os
 import sys
 
+POWER_ICONS = {'power':'','reboot':'','lock':'', 'logout':''}
+
 def setupMonitors():
     try:
         o = subprocess.check_output(['xrandr']).decode()
@@ -59,7 +61,9 @@ if __name__ == '__main__':
                 'urgentbg':'#000000','urgentfg':'#000000',
                 'focusedbg':'#000000','focusedfg':'#000000',
                 'leftmoduleprefix':"",'leftmodulesuffix':"",
-                'background':'#00000000', 'i3wspadding':0}
+                'rightmoduleprefix':"",'rightmodulesuffix':"",
+                'background':'#00000000', 'i3wspadding':0,
+                'titlepadding':0}
         for i, l in enumerate(f):
             l=l.strip()
             if l.startswith('#'):
@@ -71,8 +75,13 @@ if __name__ == '__main__':
     poly_vars = {}
     try:
         vars['i3wspadding'] = int(vars['i3wspadding'])
+        vars['titlepadding'] = int(vars['titlepadding'])
+        vars['bodypadding'] = int(vars['bodypadding'])
     except Exception as e:
         vars['i3wspadding'] = 0
+        vars['titlepadding'] = 0
+        vars['bodypadding'] = 0
+    # i3 workspace widgets
     poly_vars['focused'] = '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}%index% %icon%{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
             vars['focusedbg'],vars['leftmoduleprefix'],vars['focusedbg'],vars['focusedfg'],
             " "*vars['i3wspadding']," "*vars['i3wspadding'],vars['background'],vars['focusedbg'],
@@ -89,6 +98,43 @@ if __name__ == '__main__':
             vars['urgentbg'],vars['leftmoduleprefix'],vars['urgentbg'],vars['urgentfg'],
             " "*vars['i3wspadding']," "*vars['i3wspadding'],vars['background'],
             vars['urgentbg'],vars['leftmodulesuffix'])
+    # power menu widgets
+    poly_vars['poweropen']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['titlebg'],vars['rightmoduleprefix'],vars['titlebg'],vars['titlefg'],
+            " "*vars['titlepadding'],POWER_ICONS['power']," "*vars['titlepadding'],vars['background'],
+            vars['titlebg'],vars['rightmodulesuffix'])
+    poly_vars['powerclose']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['titlebg'],vars['rightmoduleprefix'],vars['titlebg'],vars['titlefg'],
+            " "*vars['titlepadding']," "*vars['titlepadding'],vars['background'],
+            vars['titlebg'],vars['rightmodulesuffix'])
+    poly_vars['power00']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['reboot']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
+    poly_vars['power01']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['power']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
+    poly_vars['power02']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['logout']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
+    poly_vars['power03']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['lock']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
+    poly_vars['power10']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['reboot']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
+    poly_vars['power20']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['power']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
+    poly_vars['power30']= '%[B{}]%[F{}]{}%[F-]%[B-]%[B{}]%[F{}]{}{}{}%[F-]%[B-]%[B{}]%[F{}]{}%[F-]%[B-]'.format(vars['background'],
+            vars['bodybg'],vars['rightmoduleprefix'],vars['bodybg'],vars['bodyfg'],
+            " "*vars['bodypadding'],POWER_ICONS['logout']," "*vars['bodypadding'],vars['background'],
+            vars['bodybg'],vars['rightmodulesuffix'])
     for i in poly_vars:
         poly_vars[i] = poly_vars[i].replace('[','{')
         poly_vars[i] = poly_vars[i].replace(']','}')
@@ -101,6 +147,15 @@ if __name__ == '__main__':
             os.environ['POLY_VISIBLE'] = poly_vars['visible']
             os.environ['POLY_URGENT'] = poly_vars['urgent']
             os.environ['POLY_THEME_FILE'] = theme_path
+            os.environ['POLY_POWER_OPEN'] = poly_vars['poweropen']
+            os.environ['POLY_POWER_CLOSE'] = poly_vars['powerclose']
+            os.environ['POLY_POWER_0-0'] = poly_vars['power00']
+            os.environ['POLY_POWER_0-1'] = poly_vars['power01']
+            os.environ['POLY_POWER_0-2'] = poly_vars['power02']
+            os.environ['POLY_POWER_0-3'] = poly_vars['power03']
+            os.environ['POLY_POWER_1-0'] = poly_vars['power10']
+            os.environ['POLY_POWER_2-0'] = poly_vars['power20']
+            os.environ['POLY_POWER_3-0'] = poly_vars['power30']
             if sys.version_info[0] < 3:
                 subprocess.call(['killall', '-q', 'polybar'])
                 subprocess.call(['polybar', '--reload', 'island'])
