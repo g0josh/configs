12a72e9c84ee18c7e65b57b3c1a5549b52d6ac7d
job <jobin.jj@gmail.com>
2019-10-27 00:02:22 +1100

    more alacritty colors

--- .config/alacritty/color_schemes.py
+++ .config/alacritty/color_schemes.py
@@ -5,7 +5,11 @@ import os
 import yaml
 
 ALACRITTY_CONF_PATH = os.path.expanduser('~/.config/alacritty/alacritty.yml')
-
+NAMES = ["Sea Shells", "Crayon Pony Fish", "Royal",
+        "Gooey", "Gotham", "Wryan", "Wild Cherry", "Grape",
+        "Spacedust", "Oceanic Next", "Fistank", "One Half Black",
+        "Elio", "Dracula", "Cobalt", "Ciapre", "feathers"
+        "Pencil Light", "One Light", "Hemisu Light"]
 def get_colors(name="Sea Shells"):
     if name == "Sea Shells":
         COLOR_01="#17384c"           # HOST
@@ -30,6 +34,166 @@ def get_colors(name="Sea Shells"):
         FOREGROUND_COLOR="#deb88d"   # Text
         CURSOR_COLOR=FOREGROUND_COLOR # Cursor
         PROFILE_NAME="Sea Shells"
+    elif name == "feathers":
+        COLOR_01= "#011234"
+        COLOR_02= "#cf3d38"
+        COLOR_03= "#a85a57"
+        COLOR_04= "#c85537"
+        COLOR_05= "#004588"
+        COLOR_06= "#933e3e"
+        COLOR_07= "#0b8191"
+        COLOR_08= "#707880"
+                 
+        COLOR_09= "#002459"
+        COLOR_10= "#fd6c67"
+        COLOR_11= "#fd6c67"
+        COLOR_12= "#ef8455"
+        COLOR_13= "#29a4c0"
+        COLOR_14= "#b76f6f"
+        COLOR_15= "#00c6cf"
+        COLOR_16= "#c5c8c6"
+        BACKGROUND_COLOR="#000212"   # Background Color
+        FOREGROUND_COLOR="#fcd7cf"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="feathers"
+    elif name == "Cobalt":
+        COLOR_01="#000000"           # HOST
+        COLOR_02="#ff0000"           # SYNTAX_STRING
+        COLOR_03="#38de21"           # COMMAND
+        COLOR_04="#ffe50a"           # COMMAND_COLOR2
+        COLOR_05="#1460d2"           # PATH
+        COLOR_06="#ff005d"           # SYNTAX_VAR
+        COLOR_07="#00bbbb"           # PROMP
+        COLOR_08="#bbbbbb"           #
+
+        COLOR_09="#555555"           #
+        COLOR_10="#f40e17"           # COMMAND_ERROR
+        COLOR_11="#3bd01d"           # EXEC
+        COLOR_12="#edc809"           #
+        COLOR_13="#5555ff"           # FOLDER
+        COLOR_14="#ff55ff"           #
+        COLOR_15="#6ae3fa"           #
+        COLOR_16="#ffffff"           #
+
+        BACKGROUND_COLOR="#132738"   # Background Color
+        FOREGROUND_COLOR="#ffffff"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Cobalt 2"
+    elif name =="Ciapre":
+        COLOR_01="#181818"           # HOST
+        COLOR_02="#810009"           # SYNTAX_STRING
+        COLOR_03="#48513b"           # COMMAND
+        COLOR_04="#cc8b3f"           # COMMAND_COLOR2
+        COLOR_05="#576d8c"           # PATH
+        COLOR_06="#724d7c"           # SYNTAX_VAR
+        COLOR_07="#5c4f4b"           # PROMP
+        COLOR_08="#aea47f"           #
+
+        COLOR_09="#555555"           #
+        COLOR_10="#ac3835"           # COMMAND_ERROR
+        COLOR_11="#a6a75d"           # EXEC
+        COLOR_12="#dcdf7c"           #
+        COLOR_13="#3097c6"           # FOLDER
+        COLOR_14="#d33061"           #
+        COLOR_15="#f3dbb2"           #
+        COLOR_16="#f4f4f4"           #
+
+        BACKGROUND_COLOR="#191c27"   # Background Color
+        FOREGROUND_COLOR="#aea47a"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Ciapre"
+    elif name == "Pencil Light":
+        COLOR_01="#212121"           # HOST
+        COLOR_02="#c30771"           # SYNTAX_STRING
+        COLOR_03="#10a778"           # COMMAND
+        COLOR_04="#a89c14"           # COMMAND_COLOR2
+        COLOR_05="#008ec4"           # PATH
+        COLOR_06="#523c79"           # SYNTAX_VAR
+        COLOR_07="#20a5ba"           # PROMP
+        COLOR_08="#d9d9d9"           #
+
+        COLOR_09="#424242"           #
+        COLOR_10="#fb007a"           # COMMAND_ERROR
+        COLOR_11="#5fd7af"           # EXEC
+        COLOR_12="#f3e430"           #
+        COLOR_13="#20bbfc"           # FOLDER
+        COLOR_14="#6855de"           #
+        COLOR_15="#4fb8cc"           #
+        COLOR_16="#f1f1f1"           #
+
+        BACKGROUND_COLOR="#f1f1f1"   # Background Color
+        FOREGROUND_COLOR="#424242"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Pencil Light"
+    elif name == "One Light":
+        COLOR_01="#000000"
+        COLOR_02="#DA3E39"
+        COLOR_03="#41933E"
+        COLOR_04="#855504"
+        COLOR_05="#315EEE"
+        COLOR_06="#930092"
+        COLOR_07="#0E6FAD"
+        COLOR_08="#8E8F96"
+
+        COLOR_09="#2A2B32"
+        COLOR_10="#DA3E39"
+        COLOR_11="#41933E"
+        COLOR_12="#855504"
+        COLOR_13="#315EEE"
+        COLOR_14="#930092"
+        COLOR_15="#0E6FAD"
+        COLOR_16="#FFFEFE"
+
+        BACKGROUND_COLOR="#F8F8F8"
+        FOREGROUND_COLOR="#2A2B32"
+        CURSOR_COLOR="#2A2B32"
+        PROFILE_NAME="One Light"
+    elif name == "Hemisu Light":
+        COLOR_01="#777777"
+        COLOR_02="#FF0055"
+        COLOR_03="#739100"
+        COLOR_04="#503D15"
+        COLOR_05="#538091"
+        COLOR_06="#5B345E"
+        COLOR_07="#538091"
+        COLOR_08="#999999"
+
+        COLOR_09="#999999"
+        COLOR_10="#D65E76"
+        COLOR_11="#9CC700"
+        COLOR_12="#947555"
+        COLOR_13="#9DB3CD"
+        COLOR_14="#A184A4"
+        COLOR_15="#85B2AA"
+        COLOR_16="#BABABA"
+
+        BACKGROUND_COLOR="#EFEFEF"
+        FOREGROUND_COLOR="#444444"
+        CURSOR_COLOR="#FF0054"
+        PROFILE_NAME="Hemisu Light"
+    elif name  == "One Half Black":
+        COLOR_01="#282c34"           # HOST
+        COLOR_02="#e06c75"           # SYNTAX_STRING
+        COLOR_03="#98c379"           # COMMAND
+        COLOR_04="#e5c07b"           # COMMAND_COLOR2
+        COLOR_05="#61afef"           # PATH
+        COLOR_06="#c678dd"           # SYNTAX_VAR
+        COLOR_07="#56b6c2"           # PROMP
+        COLOR_08="#dcdfe4"           #
+
+        COLOR_09="#282c34"           #
+        COLOR_10="#e06c75"           # COMMAND_ERROR
+        COLOR_11="#98c379"           # EXEC
+        COLOR_12="#e5c07b"           #
+        COLOR_13="#61afef"           # FOLDER
+        COLOR_14="#c678dd"           #
+        COLOR_15="#56b6c2"           #
+        COLOR_16="#dcdfe4"           #
+
+        BACKGROUND_COLOR="#000000"   # Background Color
+        FOREGROUND_COLOR="#dcdfe4"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="One Half Black"
     elif name == "Crayon Pony Fish":
         COLOR_01="#2b1b1d"           # HOST
         COLOR_02="#91002b"           # SYNTAX_STRING
@@ -53,7 +217,7 @@ def get_colors(name="Sea Shells"):
         FOREGROUND_COLOR="#68525a"   # Text
         CURSOR_COLOR=FOREGROUND_COLOR # Cursor
         PROFILE_NAME="Crayon Pony Fish"
-    elif name == "gooey":
+    elif name == "Gooey":
         COLOR_01="#000009"           # Black
         COLOR_02="#BB4F6C"           # Red
         COLOR_03="#72CCAE"           # Green
@@ -76,7 +240,7 @@ def get_colors(name="Sea Shells"):
         FOREGROUND_COLOR="#EBEEF9"   # Foreground Color (text)
         CURSOR_COLOR=FOREGROUND_COLOR # Cursor color
         PROFILE_NAME="gooey"
-    elif name == "gotham":
+    elif name == "Gotham":
         COLOR_01="#0a0f14" # Base 00 - Black
         COLOR_02="#c33027" # Base 08 - Red
         COLOR_03="#26a98b" # Base 0B - Green
@@ -191,6 +355,144 @@ def get_colors(name="Sea Shells"):
         FOREGROUND_COLOR="#dafaff"   # Text
         CURSOR_COLOR=FOREGROUND_COLOR # Cursor
         PROFILE_NAME="Wild Cherry"
+    elif name == "Grape":
+        COLOR_01="#2d283f"           # HOST
+        COLOR_02="#ed2261"           # SYNTAX_STRING
+        COLOR_03="#1fa91b"           # COMMAND
+        COLOR_04="#8ddc20"           # COMMAND_COLOR2
+        COLOR_05="#487df4"           # PATH
+        COLOR_06="#8d35c9"           # SYNTAX_VAR
+        COLOR_07="#3bdeed"           # PROMP
+        COLOR_08="#9e9ea0"           #
+
+        COLOR_09="#59516a"           #
+        COLOR_10="#f0729a"           # COMMAND_ERROR
+        COLOR_11="#53aa5e"           # EXEC
+        COLOR_12="#b2dc87"           #
+        COLOR_13="#a9bcec"           # FOLDER
+        COLOR_14="#ad81c2"           #
+        COLOR_15="#9de3eb"           #
+        COLOR_16="#a288f7"           #
+
+        BACKGROUND_COLOR="#171423"   # Background Color
+        FOREGROUND_COLOR="#9f9fa1"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Grape"
+    elif name == "Spacedust":
+        COLOR_01="#6e5346"           # HOST
+        COLOR_02="#e35b00"           # SYNTAX_STRING
+        COLOR_03="#5cab96"           # COMMAND
+        COLOR_04="#e3cd7b"           # COMMAND_COLOR2
+        COLOR_05="#0f548b"           # PATH
+        COLOR_06="#e35b00"           # SYNTAX_VAR
+        COLOR_07="#06afc7"           # PROMP
+        COLOR_08="#f0f1ce"           #
+
+        COLOR_09="#684c31"           #
+        COLOR_10="#ff8a3a"           # COMMAND_ERROR
+        COLOR_11="#aecab8"           # EXEC
+        COLOR_12="#ffc878"           #
+        COLOR_13="#67a0ce"           # FOLDER
+        COLOR_14="#ff8a3a"           #
+        COLOR_15="#83a7b4"           #
+        COLOR_16="#fefff1"           #
+
+        BACKGROUND_COLOR="#0a1e24"   # Background Color
+        FOREGROUND_COLOR="#ecf0c1"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Spacedust"
+    elif name == "Oceanic Next":
+        COLOR_01="#121C21"           # HOST
+        COLOR_02="#E44754"           # SYNTAX_STRING
+        COLOR_03="#89BD82"           # COMMAND
+        COLOR_04="#F7BD51"           # COMMAND_COLOR2
+        COLOR_05="#5486C0"           # PATH
+        COLOR_06="#B77EB8"           # SYNTAX_VAR
+        COLOR_07="#50A5A4"           # PROMP
+        COLOR_08="#FFFFFF"           #
+
+        COLOR_09="#52606B"           #
+        COLOR_10="#E44754"           # COMMAND_ERROR
+        COLOR_11="#89BD82"           # EXEC
+        COLOR_12="#F7BD51"           #
+        COLOR_13="#5486C0"           # FOLDER
+        COLOR_14="#B77EB8"           #
+        COLOR_15="#50A5A4"           #
+        COLOR_16="#FFFFFF"           #
+
+        BACKGROUND_COLOR="#121b21"   # Background Color
+        FOREGROUND_COLOR="#b3b8c3"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Oceanic Next"
+    elif name == "Fishtank":
+        COLOR_01="#03073c"           # HOST
+        COLOR_02="#c6004a"           # SYNTAX_STRING
+        COLOR_03="#acf157"           # COMMAND
+        COLOR_04="#fecd5e"           # COMMAND_COLOR2
+        COLOR_05="#525fb8"           # PATH
+        COLOR_06="#986f82"           # SYNTAX_VAR
+        COLOR_07="#968763"           # PROMP
+        COLOR_08="#ecf0fc"           #
+
+        COLOR_09="#6c5b30"           #
+        COLOR_10="#da4b8a"           # COMMAND_ERROR
+        COLOR_11="#dbffa9"           # EXEC
+        COLOR_12="#fee6a9"           #
+        COLOR_13="#b2befa"           # FOLDER
+        COLOR_14="#fda5cd"           #
+        COLOR_15="#a5bd86"           #
+        COLOR_16="#f6ffec"           #
+
+        BACKGROUND_COLOR="#232537"   # Background Color
+        FOREGROUND_COLOR="#ecf0fe"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Fishtank"
+    elif name == "Elio":
+        COLOR_01="#303030"           # HOST
+        COLOR_02="#e1321a"           # SYNTAX_STRING
+        COLOR_03="#6ab017"           # COMMAND
+        COLOR_04="#ffc005"           # COMMAND_COLOR2
+        COLOR_05="#729FCF"           # PATH
+        COLOR_06="#ec0048"           # SYNTAX_VAR
+        COLOR_07="#2aa7e7"           # PROMP
+        COLOR_08="#f2f2f2"           #
+
+        COLOR_09="#5d5d5d"           #
+        COLOR_10="#ff361e"           # COMMAND_ERROR
+        COLOR_11="#7bc91f"           # EXEC
+        COLOR_12="#ffd00a"           #
+        COLOR_13="#0071ff"           # FOLDER
+        COLOR_14="#ff1d62"           #
+        COLOR_15="#4bb8fd"           #
+        COLOR_16="#a020f0"           #
+
+        BACKGROUND_COLOR="#041A3B"   # Background Color
+        FOREGROUND_COLOR="#f2f2f2"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Elio"
+    elif name == "Dracula":
+        COLOR_01="#44475a"           # HOST
+        COLOR_02="#ff5555"           # SYNTAX_STRING
+        COLOR_03="#50fa7b"           # COMMAND
+        COLOR_04="#ffb86c"           # COMMAND_COLOR2
+        COLOR_05="#8be9fd"           # PATH
+        COLOR_06="#bd93f9"           # SYNTAX_VAR
+        COLOR_07="#ff79c6"           # PROMP
+        COLOR_08="#94A3A5"           #
+
+        COLOR_09="#000000"           #
+        COLOR_10="#ff5555"           # COMMAND_ERROR
+        COLOR_11="#50fa7b"           # EXEC
+        COLOR_12="#ffb86c"           #
+        COLOR_13="#8be9fd"           # FOLDER
+        COLOR_14="#bd93f9"           #
+        COLOR_15="#ff79c6"           #
+        COLOR_16="#ffffff"           #
+
+        BACKGROUND_COLOR="#282a36"   # Background Color
+        FOREGROUND_COLOR="#94A3A5"   # Text
+        CURSOR_COLOR=FOREGROUND_COLOR # Cursor
+        PROFILE_NAME="Dracula"
     else:
         print("Unknown color scheme")
         return False
@@ -218,13 +520,14 @@ def get_colors(name="Sea Shells"):
     return colors
 
 def main():
+    global COLORS
     if len(sys.argv) < 2:
         print("Please enter a theme name")
         sys.exit(1)
 
     colors = get_colors(sys.argv[1].strip())
     if not colors:
-        print("Unknown theme")
+        print("Unknown theme, Acceptable ones are - ", NAMES)
         sys.exit(1)
     with open(ALACRITTY_CONF_PATH, 'r') as fh:
         ala_conf = yaml.safe_load(fh)
