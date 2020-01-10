93a5792f5c6d18bfac8c5dd5f485f8c3d7e8733f
job <jobin.jj@gmail.com>
2020-01-10 21:53:49 +1100

    scripts moved to pycliutils

--- installqtile.sh
+++ installqtile.sh
@@ -8,23 +8,6 @@ fi
 read -p 'Office(1) or Home(0): ' office
 
 sudo apt install xorg
-echo ""
-echo "---------------------------------------"
-echo "Installing compton"
-echo "---------------------------------------"
-echo ""
-sudo apt install compton
-
-# compton
-#sudo apt install asciidoc --no-install-recommends
-#sudo apt install libxinerama-dev libxrandr-dev libxcomposite-dev libdbus-1-dev libconfig9 docbook-xml libxml2-utils xsltproc libconfig-dev libxslt1-dev docbook-xsl
-#cd ~/Downloads
-#git clone https://github.com/tryone144/compton.git
-#cd compton
-#make
-#make docs
-#sudo make install
-#sudo apt remove libxinerama-dev libxrandr-dev libxcomposite-dev libdbus-1-dev docbook-xml libxml2-utils xsltproc asciidoc libxslt1-dev libconfig-dev docbook-xsl
 
 echo ""
 echo "---------------------------------------"
