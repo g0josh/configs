e4f4632a92625426f32524f39481c820ac995669
job <jobin.jj@gmail.com>
2020-11-21 23:21:59 +1100

    pop os

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
 killall compton &
+
+# merge theme
+theme=$HOME/.config/themes/.xcolors
+if [ -f $theme ]; then
+    xrdb -merge $theme
+fi
+
 numlockx on
 udiskie -2 &
 urxvtd &
 compton &
+reload-screens
