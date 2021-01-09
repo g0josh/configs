79fb83d7874125dd928aa4f0687873dea7611eb3
Job <jobin.jj@gmail.com>
2021-01-09 22:59:21 +1100

    autostart resilio

--- .config/autostart.sh
+++ .config/autostart.sh
@@ -9,6 +9,7 @@ fi
 numlockx on
 udiskie -2 &
 urxvtd &
+rslync --config ~/.config/resilio-sync/config.json 
 picom --experimental-backends &
 urxvt -e musikcube &
 mcc --updated-callback 'polybar-msg hook musik 1' &
